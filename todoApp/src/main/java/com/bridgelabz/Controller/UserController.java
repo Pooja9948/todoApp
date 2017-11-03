625f17cd9cca8664df2d29fbd1a5d5c1763ae3e6
Pooja Nayak <poojanayak.1994@gmail.com>
2017-11-03 20:58:45 +0530

    working on token

--- todoApp/src/main/java/com/bridgelabz/Controller/UserController.java
+++ todoApp/src/main/java/com/bridgelabz/Controller/UserController.java
@@ -1,21 +1,21 @@
 package com.bridgelabz.Controller;
 
-import java.util.List;
-
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailSender;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.Mail.SendMail;
+import com.bridgelabz.model.Token;
 import com.bridgelabz.model.UserDetails;
 import com.bridgelabz.service.UserService;
+import com.bridgelabz.token.GenerateToken;
 import com.bridgelabz.validation.Validator;
 
 /**
@@ -35,6 +35,9 @@ public class UserController{
 	@Autowired
 	SendMail  sendmail;
 	
+	@Autowired
+	Token token;
+	
 	@RequestMapping(value="/registrationForm", method= RequestMethod.POST)
 	public ResponseEntity<String> registrationUser(@RequestBody UserDetails user) {
 		String isValidate = validator.validateSaveUser(user);
@@ -50,10 +53,14 @@ public class UserController{
 	}
 	
 	@RequestMapping(value="/login", method= RequestMethod.POST)
-	public ResponseEntity<String> loginUser(@RequestBody UserDetails user,HttpSession session) {
+	public ResponseEntity<String> loginUser(@RequestBody UserDetails user,HttpServletRequest request,HttpSession session) {
 		System.out.println("email "+user.getEmail()+" password "+user.getPassword());
 		user=userservice.loginUser(user);
 		session.setAttribute("user", user);
+		String accessToken = GenerateToken.generateToken(user.getId());
+		token.setGenerateToken(accessToken);
+		String url = request.getRequestURL().toString();
+		url = url.substring(0, url.lastIndexOf("/")) + "/" + "finalLogin" + "/" + accessToken;
 		if(user != null){
 			System.out.println("login successful!!!");
 			return new ResponseEntity<String> (HttpStatus.OK);
---
+++ todoApp/src/main/java/com/bridgelabz/token/GenerateToken.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.token;
+
+import java.util.Date;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class GenerateToken {
+	
+	private static final String key="todoToken";
+	
+	public static String generateToken(int userId){
+		Date atTokenCreationTime = new Date();
+		Date atExpirationTime = new Date(atTokenCreationTime.getTime()+1000*60*5);
+		SignatureAlgorithm signatureAlogirthm = SignatureAlgorithm.HS512;
+		JwtBuilder builder = Jwts.builder();
+		builder.setSubject("accessToken");
+		builder.setIssuedAt(atTokenCreationTime);
+		builder.setExpiration(atExpirationTime);
+		builder.setIssuer(String.valueOf(userId));
+		builder.signWith(signatureAlogirthm, key);
+		String jwtBuilder = builder.compact();
+		return jwtBuilder;
+	}
+}
